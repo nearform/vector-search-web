245403b1770304b2d93934369db25199abfe737f
Ryan Roemer <ryan.roemer@nearform.com>
2026-01-13 14:50:08 -0800

    Clean up data init

--- public/app/data/index.js
+++ public/app/data/index.js
@@ -1,7 +1,35 @@
 // Public API for data layer
-export { searchPosts, getExtractor, getChunksDb } from "./search.js";
-export {
+import { searchPosts, getExtractor, getChunksDb } from "./search.js";
+import {
   getPosts,
   getPostsEmbeddings256,
   getPostsEmbeddings512,
 } from "./api.js";
+
+/**
+ * Start preloading all data and models in parallel.
+ * Returns a Promise.all that resolves when everything is ready.
+ * Callers can fire-and-forget or await if they need to wait.
+ *
+ * Errors are cached by getAndCache and will surface when searchPosts
+ * awaits the failed promise, propagating to handleSubmit's setError.
+ */
+const initData = () =>
+  Promise.all([
+    getPosts(),
+    getPostsEmbeddings256(),
+    getPostsEmbeddings512(),
+    getExtractor(),
+    getChunksDb(256),
+    getChunksDb(512),
+  ]);
+
+export {
+  initData,
+  searchPosts,
+  getExtractor,
+  getChunksDb,
+  getPosts,
+  getPostsEmbeddings256,
+  getPostsEmbeddings512,
+};
